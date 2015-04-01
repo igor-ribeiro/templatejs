e2d02e015269a53a84f0465aad145c4e6ade1d9d
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-04-01 16:58:00 -0300

    Add data-click functionality

--- js/template.js
+++ js/template.js
@@ -2,25 +2,61 @@
   'use strict';
 
   win.Template = function () {
-    this.controllers = {};
-  }
+    this.$controllers = {};
+    this.$elements    = [];
+  };
+
+  Template.prototype.clickHadler = function (controllerElement) {
+    var self       = this;
+    var controller = this.$controllers[controllerElement.dataset.controller];
+    var elements   = document.querySelectorAll('[data-click]');
+
+    controllerElement.addEventListener('click', function (event) {
+      var target = event.target;
+
+      [].forEach.call(elements, function (element) {
+        if (element === target) {
+          var command = ['controller.', element.dataset.click].join('');
+
+          eval(command);
+        }
+      });
+    });
+
+    // [].forEach.call(elements, function (element) {
+    //   element.addEventListener('click', function () {
+    //     eval('controller.' + element.dataset.click);
+    //   })
+    // })
+  };
+
+  Template.prototype.repeatHandler = function () {
+
+  };
 
   Template.prototype.controller = function (controllerName, controllerFunction) {
-    this.controllers[controllerName] = controllerFunction;
+    this.$controllers[controllerName] = controllerFunction;
 
-    var controller = this.controllers[controllerName]();
+    var controller        = this.$controllers[controllerName];
+    var selector          = ['[data-controller="', controllerName, '"]'].join('');
+    var controllerElement = document.querySelector(selector);
 
-  }
+    this.$elements.push(controllerElement);
+
+    this.clickHadler(controllerElement);
+  };
 
 }(window));
 
 var app = new Template();
 
-app.controller('filesController', function () {
-    this.files = ['index.html', 'javascript.js', 'style.css'];
-
-    return this;
-  });
+app.controller('filesController', {
+  files: ['index.html', 'app.js', 'styles.css'],
+  
+  open: function () {
+    console.log(this.files);
+  }
+});
 
 
 
